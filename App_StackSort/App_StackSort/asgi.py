768224f629efff3938f035fa2234879bc9cf5c33
Elena Maksimova <elmaksim@c2r5s1.42yerevan.am>
2024-01-19 14:02:19 +0400

    new

--- App_StackSort/App_StackSort/asgi.py
+++ App_StackSort/App_StackSort/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for App_Stack_sort project.
+ASGI config for App_StackSort project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App_Stack_sort.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App_StackSort.settings')
 
 application = get_asgi_application()
--- App_StackSort/App_StackSort/urls.py
+++ App_StackSort/App_StackSort/urls.py
@@ -1,4 +1,4 @@
-"""App_Stack_sort URL Configuration
+"""App_StackSort URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
